f173496770fa813dd2d9f3de0dd6d6ae7174eec4
Micha Niskin <asdf@ubergibson.com>
2009-12-29 02:36:01 -0500

    simple component example

--- controller.js
+++ controller.js
@@ -1,9 +1,7 @@
 $.golf.controller = [
   { route: ".*",
     action: function(container, params) {
-      container.empty().append(
-        "<h1>Hello, world! This is "+params[0]+"!</h1>"
-      );
+      container.empty().append(new Component.HelloWorld());
     }
   }
 ];
